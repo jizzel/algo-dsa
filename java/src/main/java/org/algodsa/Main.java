0294e3d4818825def930eaa7ff9117a2ee0b6e61
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-08-24 15:27:00 +0000

    chore: directory restructure

--- java/src/main/java/org/algodsa/Main.java
+++ java/src/main/java/org/algodsa/Main.java
@@ -1,6 +1,7 @@
+package main.java.org.algodsa;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-
 /**
  * Main Class
  */
