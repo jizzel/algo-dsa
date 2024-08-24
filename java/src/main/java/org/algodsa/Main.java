c7becab701a1dcfcd1b2e0ce085cad1e8cc5f1e7
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-08-24 16:25:04 +0000

    refactor: remove print statements and update test cases for addBinary

--- java/src/main/java/org/algodsa/Main.java
+++ java/src/main/java/org/algodsa/Main.java
@@ -22,17 +22,5 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
-        System.out.println(Solution.titleToNumber("ZY"));
-        System.out.println(Solution.lengthOfLastWord("Hello World"));
-        System.out.println(Solution.strStr("sadbutsad", "sad"));
-        System.out.println(Solution.repeatedSubstringPattern("abab"));
-        System.out.println(Solution.toLowerCase("Hello"));
-        System.out.println(Solution.fizzBuzz(3));
-        System.out.println(Solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));
-        System.out.println(Solution.detectCapitalUse("FFFf"));
-        System.out.println(Solution.wordPattern("abba", "dog cat cat dog"));
-        System.out.println(Solution.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
-
     }
 }
