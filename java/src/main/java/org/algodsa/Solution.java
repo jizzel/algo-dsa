f00ec79e10acc656d2bf17e176e87d38eb350316
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-08-27 23:46:20 +0000

    feat: add binary tree traversals (preorder, inorder, postorder)

--- java/src/main/java/org/algodsa/Solution.java
+++ java/src/main/java/org/algodsa/Solution.java
@@ -412,4 +412,104 @@ public class Solution {
         return result.reverse().toString();
     }
 
+    /**
+     * <h4><a href="https://leetcode.com/problems/binary-tree-preorder-traversal/">Binary Tree Preorder Traversal</a></h4>
+     * Performs a preorder traversal on a binary tree. <br>
+     * <br>
+     * In preorder traversal, the nodes are recursively visited in this order:<br>
+     * 1. Visit the current node.<br>
+     * 2. Traverse the left subtree.<br>
+     * 3. Traverse the right subtree.<br>
+     * <br>
+     * The method uses a helper function `preOrder` to recursively traverse the tree
+     * and add the values of the nodes to a result list.<br>
+     * <br>
+     * Time Complexity: O(n) - where n is the number of nodes in the tree. Each node is visited once.<br>
+     * Space Complexity: O(n) - for the result list. The recursion stack space is O(h), where h is the height of the tree.<br>
+     * <br>
+     * @param root the root node of the binary tree
+     * @return a list of integers representing the values of the nodes in preorder
+     */
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return preOrder(root, result);
+    }
+
+    private static List<Integer> preOrder(TreeNode node, List<Integer> result) {
+        if (node == null) return result;
+
+        result.add(node.val);         // Visit the current node
+        preOrder(node.left, result);  // Traverse the left subtree
+        preOrder(node.right, result); // Traverse the right subtree
+
+        return result;
+    }
+
+    /**
+     * <h4><a href="https://leetcode.com/problems/binary-tree-inorder-traversal/description/">Binary Tree Inorder Traversal</a></h4>
+     * Performs an inorder traversal on a binary tree.<br>
+     * <br>
+     * In inorder traversal, the nodes are recursively visited in this order:<br>
+     * 1. Traverse the left subtree.<br>
+     * 2. Visit the current node.<br>
+     * 3. Traverse the right subtree.<br>
+     * <br>
+     * The method uses a helper function `inorder` to recursively traverse the tree
+     * and add the values of the nodes to a result list.<br>
+     * <br>
+     * Time Complexity: O(n) - where n is the number of nodes in the tree. Each node is visited once.<br>
+     * Space Complexity: O(n) - for the result list. The recursion stack space is O(h), where h is the height of the tree.<br>
+     * <br>
+     * @param root the root node of the binary tree
+     * @return a list of integers representing the values of the nodes in inorder
+     */
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return inorder(root, result);
+    }
+
+    private static List<Integer> inorder(TreeNode node, List<Integer> res) {
+        if (node == null) return res;
+
+        inorder(node.left, res);  // Traverse the left subtree
+        res.add(node.val);        // Visit the current node
+        inorder(node.right, res); // Traverse the right subtree
+
+        return res;
+    }
+
+    /**
+     * <h4><a href="https://leetcode.com/problems/binary-tree-postorder-traversal/description/">Binary Tree Postorder Traversal</a></h4>
+     * Performs a postorder traversal on a binary tree.<br>
+     * <br>
+     * In postorder traversal, the nodes are recursively visited in this order:<br>
+     * 1. Traverse the left subtree.<br>
+     * 2. Traverse the right subtree.<br>
+     * 3. Visit the current node.<br>
+     * <br>
+     * The method uses a helper function `postOrder` to recursively traverse the tree
+     * and add the values of the nodes to a result list.<br>
+     * <br>
+     * Time Complexity: O(n) - where n is the number of nodes in the tree. Each node is visited once.
+     * Space Complexity: O(n) - for the result list. The recursion stack space is O(h), where h is the height of the tree.
+     *
+     * @param root the root node of the binary tree
+     * @return a list of integers representing the values of the nodes in postorder
+     */
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return postOrder(root, result);
+    }
+
+    private static List<Integer> postOrder(TreeNode node, List<Integer> result) {
+        if (node == null) return result;
+
+        postOrder(node.left, result);  // Traverse the left subtree
+        postOrder(node.right, result); // Traverse the right subtree
+        result.add(node.val);          // Visit the current node
+
+        return result;
+    }
+
+
 }
