cdc8291ade5bd4baed4e45e6a0c220149507b44d
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-08-27 22:35:18 +0000

    fix: add comments

--- java/src/main/java/org/algodsa/TreeNode.java
+++ java/src/main/java/org/algodsa/TreeNode.java
@@ -1,19 +1,33 @@
 package main.java.org.algodsa;
 
 /**
- * Tree node blueprint
+ * Represents a node in a binary tree.
+ * Each node contains a value and references to its left and right children.
  */
 public class TreeNode {
+    /**
+     * The value stored in the node.
+     */
     int val;
-    TreeNode left, right;
 
     /**
-     * Constructor
+     * Reference to the left child node.
+     */
+    TreeNode left;
+
+    /**
+     * Reference to the right child node.
+     */
+    TreeNode right;
+
+    /**
+     * Constructs a TreeNode with a specified value.
+     * The left and right child nodes are initialized to null.
+     *
      * @param val the value of the node
      */
     TreeNode(int val){
         this.val = val;
         this.left = this.right = null;
     }
-
 }
