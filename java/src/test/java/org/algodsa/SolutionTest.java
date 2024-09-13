0f4f0547719ea84f365fec7e4997e4a8bf53cd30
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-09-13 09:45:17 +0000

    feat: add test for Invert Binary Tree

--- java/src/test/java/org/algodsa/SolutionTest.java
+++ java/src/test/java/org/algodsa/SolutionTest.java
@@ -226,4 +226,26 @@ public class SolutionTest {
         assertEquals(5, Solution.kthLargestLevelSum(root, 2));
         assertEquals(1, Solution.kthLargestLevelSum(root, 3));
     }
+
+    @Test
+    public void testInvertTree() {
+        TreeNode root = setUp();
+        // Invert the tree
+        TreeNode invertedRoot = Solution.invertTree(root);
+
+        // Check the root is still the same
+        assertEquals(1, invertedRoot.val);
+
+        // Check if left and right children of the root are swapped
+        assertEquals(3, invertedRoot.left.val);
+        assertEquals(2, invertedRoot.right.val);
+
+        // Check if the left subtree of root (which was originally 3) is also inverted
+        assertEquals(7, invertedRoot.left.left.val);  // originally right child of 3
+        assertEquals(6, invertedRoot.left.right.val); // originally left child of 3
+
+        // Check if the right subtree of root (which was originally 2) is also inverted
+        assertEquals(5, invertedRoot.right.left.val); // originally right child of 2
+        assertEquals(4, invertedRoot.right.right.val); // originally left child of 2
+    }
 }
