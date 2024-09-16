4b7683de0ab9e8b04191a7f7a696375a5ec3fb70
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-09-16 10:24:31 +0000

    feat: add tests for Two Sum

--- java/src/test/java/org/algodsa/MiscellaneousTest.java
+++ java/src/test/java/org/algodsa/MiscellaneousTest.java
@@ -103,4 +103,28 @@ class MiscellaneousTest {
         // Reset System.out back to its original state
         System.setOut(System.out);
     }
+
+    @Test
+    public void testTwoSum() {
+        // Test case 1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = Miscellaneous.twoSum(nums1, target1);
+        assertNotNull(result1, "The result should not be null");
+        assertArrayEquals(new int[]{0, 1}, result1, "Indices should match the two numbers that sum up to the target");
+
+        // Test case 2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = Miscellaneous.twoSum(nums2, target2);
+        assertNotNull(result2, "The result should not be null");
+        assertArrayEquals(new int[]{1, 2}, result2, "Indices should match the two numbers that sum up to the target");
+
+        // Test case 3
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = Miscellaneous.twoSum(nums3, target3);
+        assertNotNull(result3, "The result should not be null");
+        assertArrayEquals(new int[]{0, 1}, result3, "Indices should match the two numbers that sum up to the target");
+    }
 }
