b1b5c4edfabe9304fd2ab38fe61bc697720535eb
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-10-10 16:44:02 +0000

    refactor

--- java/src/main/java/org/algodsa/CheckIfNAndItsDoubleExist.java
+++ java/src/main/java/org/algodsa/CheckIfNAndItsDoubleExist.java
@@ -13,16 +13,17 @@ public class CheckIfNAndItsDoubleExist {
     public CheckIfNAndItsDoubleExist(){}
 
     /**
+     * <h4><a href="https://leetcode.com/problems/check-if-n-and-its-double-exist/description/">1346. Check If N and Its Double Exist</a></h4>
      * Checks if there exists two integers N and M in the array such that N is the double of M (i.e., N = 2 * M).
-     *
+     * <br>
      * This is achieved by iterating over the array and using a HashSet to store elements, checking if the current
-     * element's half or double already exists in the set.
-     *
+     * element's half or double already exists in the set.<br>
+     * <br>
      * Time Complexity: O(n), where n is the number of elements in the array.
      * The iteration over the array takes O(n) and both the contains and add operations on the HashSet are O(1) on average.
-     *
-     * Space Complexity: O(n), as the HashSet stores at most n elements from the array.
-     *
+     * <br>
+     * Space Complexity: O(n), as the HashSet stores at most n elements from the array.<br>
+     * <br>
      * @param arr the input array of integers
      * @return true if such a pair (N, 2 * N) exists, false otherwise
      */
