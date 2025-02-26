29c04711458adf316149f536cf73d1b1944e200b
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2025-02-26 11:57:23 +0000

    feat: add custom bubble sort

--- java/src/main/java/org/algodsa/HeightChecker.java
+++ java/src/main/java/org/algodsa/HeightChecker.java
@@ -26,12 +26,29 @@ public class HeightChecker {
      */
     public int heightChecker(int[] heights) {
         int[] expected = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(expected); // ToDo: Custom
+//        Arrays.sort(expected);
+        bubbleSort(expected);
         int count = 0;
         for (int i = 0; i < heights.length; i++) {
             if (heights[i] != expected[i]) count++;
         }
         return count;
+    }
+
+    public void bubbleSort(int[] arr){
+        boolean swapped = true;
+
+        while(swapped){
+            swapped = false;
+            for(int i = 0; i < arr.length - 1; i++){
+                if(arr[i] > arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    swapped = true;
+                }
+            }
+        }
     }
 }
 
